46e9d92e6be8031eeced092a5703fe397fd580a3
John Serrano <andrey_201015@hotmail.com>
2025-01-07 21:25:38 -0500

    Init component Start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { Start } from '@/components/Start'
+
 export default function Home() {
   return (
     <>
       <div className='max-w-[1200px] mx-auto py-12'>
         <h1 className='text-3xl text-center'>Biblia Quiz</h1>
+
+        <Start />
       </div>
     </>
   )
