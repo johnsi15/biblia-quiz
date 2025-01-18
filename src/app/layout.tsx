141be90ccfb7f161d21f5d8f77f393e7e5e57e6f
John Serrano <andrey_201015@hotmail.com>
2025-01-18 12:04:59 -0500

    Changes layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import { Header } from '@/components/Header'
+import { Footer } from '@/components/Footer'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,7 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='es'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header />
+        <main className='container mx-auto px-4 py-8'>{children}</main>
+        <Footer />
+      </body>
     </html>
   )
 }
