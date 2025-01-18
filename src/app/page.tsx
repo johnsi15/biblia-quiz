3e9038cdeb6b998c0e40fdd141407914d80c07a7
John Serrano <andrey_201015@hotmail.com>
2025-01-18 12:05:09 -0500

    Changes page app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+import { ListGame } from '@/components/ListGames'
 import { Start } from '@/components/Start'
 
 export default function Home() {
   return (
-    <main className='max-w-[1200px] mx-auto py-12'>
-      <h1 className='text-3xl text-center'>Biblia Quiz</h1>
+    <section className='max-w-[1200px] mx-auto'>
+      <h1 className='font-bold text-5xl md:text-7xl text-balance text-center max-w-[20ch] mx-auto md:leading-[1.16]'>
+        Descubre la <span className='text-blue-500'>Biblia</span> Explora y Aprende
+      </h1>
 
+      <p className='text-white/80 text-center text-2xl mt-6 max-w-3xl mx-auto'>
+        Resuelve tus dudas y profundiza en la Biblia con pruebas divertidas para hacer solo, en familia, con amigos o
+        con tu grupo de jóvenes.
+      </p>
+
+      <ListGame />
       <Start />
-    </main>
+    </section>
   )
 }
