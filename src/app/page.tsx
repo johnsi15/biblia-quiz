03c78e42656d2f45962dd4929f1246c06641968a
John Serrano <andrey_201015@hotmail.com>
2025-01-19 17:07:24 -0500

    Changes styles add font to titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { Start } from '@/components/Start'
 export default function Home() {
   return (
     <section className='max-w-[1200px] mx-auto'>
-      <h1 className='font-bold text-5xl md:text-7xl text-balance text-center max-w-[20ch] mx-auto md:leading-[1.16]'>
+      <h1 className='font-mono font-bold text-5xl md:text-7xl text-balance text-center max-w-[20ch] mx-auto md:leading-[1.16]'>
         Descubre la <span className='text-blue-500'>Biblia</span> Explora y Aprende
       </h1>
 
-      <p className='text-white/80 text-center text-2xl mt-6 max-w-3xl mx-auto'>
+      <p className='text-white/80 text-center text-2xl mt-6 max-w-3xl mx-auto prose'>
         Resuelve tus dudas y profundiza en la Biblia con pruebas divertidas para hacer solo, en familia, con amigos o
         con tu grupo de jóvenes.
       </p>
